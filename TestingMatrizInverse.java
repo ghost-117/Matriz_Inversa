d9a5d75db77f9feb1f7ecf895aadab670d146f36
Jonh Look <Axel Lopez Cornejo>
2025-11-22 18:48:38 -0600

    v1.0: Agregar un metyodo void printMatrix

--- TestingMatrizInverse.java
+++ TestingMatrizInverse.java
@@ -1,3 +1,17 @@
-public class TestingMatrizInverse {
-    
+
+public class TestingMatrizInverse{
+
+    public static void printMatrix(double[][] matrix, String titulo){
+        System.out.println("\n" + titulo);
+        System.out.println("----------------------------");
+        int n = matrix.length;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                System.out.printf("%.4f\t", matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("----------------------------");
+    }
+
 }
